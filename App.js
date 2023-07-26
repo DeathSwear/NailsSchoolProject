fd917e9e2c8738708b089684e5cdd63c183bdd57
DeathSwear <vlad0septa@yandex.ru>
2023-07-27 00:03:01 +0600

    test

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+//import { View, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { Navigator } from './src/navigation/Navigator';
+import {Provider} from "react-redux";
+import { store } from './src/store/store';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+    <Provider store={store}>
+        <NavigationContainer>
+          <Navigator/>
+        </NavigationContainer>
+    </Provider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+<PersistGate loading={null} persistor={persistor}> </PresistDate> (они внутри провайдера)
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and latr, measurementId is optionale
+const firebaseConfig = {
+  apiKey: "AIzaSyAOx6IIyjKncI1ncug-CiL5SacJc9ZGZag",
+  authDomain: "nails-reactnative.firebaseapp.com",
+  projectId: "nails-reactnative",
+  storageBucket: "nails-reactnative.appspot.com",
+  messagingSenderId: "521965448859",
+  appId: "1:521965448859:web:942adbfd80d7d21c00e03e",
+  measurementId: "G-RR0YCZQEC6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);*/
